cb4d04257c4882d2038e4f0b25cd3cd7d54a5776
Aleksandar <aca.peric93@gmail.com>
2023-01-10 23:46:31 +0100

    root page

--- frontend/src/routes/root.jsx
+++ frontend/src/routes/root.jsx
@@ -1,4 +1,6 @@
 import { Outlet, Link, useLoaderData } from "react-router-dom";
+import Header from "../../components/Header";
+
 
 export async function loader() {
   try {
@@ -18,57 +20,39 @@ export default function Root() {
   const receipts = useLoaderData();
   return (
     <>
-      <div id="sidebar">
-        <h1>QR code scanner</h1>
-        <div>
-          <form id="search-form" role="search">
-            <input
-              id="q"
-              aria-label="Search receipts"
-              placeholder="Search"
-              type="search"
-              name="q"
-            />
-            <div
-              id="search-spinner"
-              aria-hidden
-              hidden={true}
-            />
-            <div
-              className="sr-only"
-              aria-live="polite"
-            ></div>
-          </form>
-          <form method="post">
-            <button type="submit">Scan</button>
-          </form>
+      <div id='container'>
+        <Header />
+        <div id='content'>
+          <div id="sidebar">
+            <h1>QR code scanner</h1>
+            <nav>
+              {receipts.length ? (
+                <ul>
+                  {receipts.map((receipt) => (
+                    <li key={receipt.id}>
+                      <Link to={`receipts/${receipt.id}`}>
+                        {receipt.store ? (
+                          <>
+                            {receipt.store} {receipt.total_price}
+                          </>
+                        ) : (
+                          <i>No Name</i>
+                        )}{" "}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              ) : (
+                <p>
+                  <i>No receipts</i>
+                </p>
+              )}
+            </nav>
+          </div>
+          <div id="detail">
+            <Outlet />
+          </div>
         </div>
-        <nav>
-          {receipts.length ? (
-            <ul>
-              {receipts.map((receipt) => (
-                <li key={receipt.id}>
-                  <Link to={`receipts/${receipt.id}`}>
-                    {receipt.store ? (
-                      <>
-                        {receipt.store} {receipt.total_price}
-                      </>
-                    ) : (
-                      <i>No Name</i>
-                    )}{" "}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          ) : (
-            <p>
-              <i>No receipts</i>
-            </p>
-          )}
-        </nav>
-      </div>
-      <div id="detail">
-        <Outlet />
       </div>
     </>
   );
