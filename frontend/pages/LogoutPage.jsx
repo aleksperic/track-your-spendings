2034b719d660a80acf96c2871e6e344ec4cd1823
Aleksandar <aca.peric93@gmail.com>
2023-01-14 23:24:27 +0100

    Logout page bug fix

--- frontend/pages/LogoutPage.jsx
+++ frontend/pages/LogoutPage.jsx
@@ -1,17 +1,23 @@
+import { useContext } from "react"
 import { useNavigate } from "react-router-dom"
+import AuthContext from "../context/AuthContext"
 
 const LogoutPage = () => {
+
+    const { logoutUser } = useContext(AuthContext)
     const navigate = useNavigate()
 
-    const logout = () => {
-        localStorage.removeItem('authTokens')
-        navigate('/login', { replace: true })
-        navigate(0)
+    const logout = async () => {
+        await logoutUser()
+        navigate('/login')
     }
+
     return (
         <div id='container'>
             <div id="logout">
-                <button onClick={logout}>Logout</button>
+                <button onClick={logout}>
+                    Logout
+                </button>
             </div>
         </div>
     )
