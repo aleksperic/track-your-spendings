c06c6633409dfa3da0353666c4a0bf01347d1ee9
Aleksandar <aca.peric93@gmail.com>
2023-01-15 19:53:12 +0100

    Login page bug fix

--- frontend/pages/LoginPage.jsx
+++ frontend/pages/LoginPage.jsx
@@ -1,56 +1,23 @@
-import React, { useState } from "react"
+import React, { useContext } from "react"
 import { useNavigate } from "react-router-dom"
-import jwt_decode from "jwt-decode";
+import AuthContext from "../context/AuthContext";
 
 const LoginPage = () => {
 
+    const { loginUser } = useContext(AuthContext)
     const navigate = useNavigate()
 
-    const [authTokens, setAuthTokens] = useState(() =>
-        localStorage.getItem('authTokens') ?
-            JSON.parse(localStorage.getItem('authTokens')) : null
-    )
-    const [user, setUser] = useState(() =>
-        localStorage.getItem('authTokens') ?
-            jwt_decode(localStorage.getItem('authTokens')) : null
-    )
-
-    const loginUser = async (e) => {
-        e.preventDefault()
-        try {
-            const response = await fetch('http://localhost:8000/api/token/', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    'username': e.target.username.value,
-                    'password': e.target.password.value
-                })
-            })
-            if (response.status === 200) {
-                const data = await response.json()
-                setAuthTokens(data)
-                setUser(jwt_decode(data.access))
-                localStorage.setItem('authTokens', JSON.stringify(data))
-                navigate('/', { replace: true })
-
-            } else if (response.status === 401) {
-                alert(response.statusText + '\nPlease provide valid credentials...')
-            } else if (response.status === 400) {
-                alert(response.statusText + '\nPlease provide valid credentials...')
-            }
-
-        } catch (error) {
-            console.error(error);
-        }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        loginUser(e.target.username.value, e.target.password.value);
+        navigate('/')
     }
 
     return (
 
         <div id='container'>
             <div id="login">
-                <form onSubmit={loginUser}>
+                <form onSubmit={handleSubmit}>
                     <input type="text" name="username" placeholder="Enter username..." />
                     <input type="password" name="password" placeholder="Enter password..." />
                     <input type="submit" />
