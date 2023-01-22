c4f15b533304fd5a48ca1839c471c336020919a1
Aleksandar <aca.peric93@gmail.com>
2023-01-22 23:46:49 +0100

    header bug fix

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -1,21 +1,14 @@
-import React, { useState, useEffect, useContext } from "react"
-import { Link } from "react-router-dom"
+import React, { useContext } from "react"
+import { Form, Link } from "react-router-dom"
 import AuthContext from "../context/AuthContext"
 
 const Header = () => {
     const { user } = useContext(AuthContext)
-    const [userId, setUserId] = useState(null)
-
-    useEffect(() => {
-        if (user) {
-            setUserId(user.username)
-        }
-    }, [user])
 
     return (
         <div id="header">
             <div>
-                <form id="search-form" role="search">
+                <Form id="search-form" role="search">
                     <input
                         id="q"
                         aria-label="Search receipts"
@@ -32,27 +25,25 @@ const Header = () => {
                         className="sr-only"
                         aria-live="polite"
                     ></div>
-                </form>
+                </Form>
                 <form method="post">
                 </form>
             </div>
             <div id="links">
-                {/* <button type="submit">Scan</button> */}
-
-
-                <Link to={'/'}><button>Home</button></Link>
-
-                <span> | </span>
-                <Link to={'/scan'}>Scan</Link>
-                <span> | </span>
-                {user ?
-                    <>
-                        <span> | </span>
-                        <Link to={'/logout'}>Logout</Link>
-                    </>
-                    : <Link to={'/login'}>Login</Link>
-                }
-                {user && <span> | Hello {user.first_name}</span>}
+                <nav>
+                    <Link to={'/'}>Home</Link>
+                    <span> | </span>
+                    <Link to={'/scan'}>Scan</Link>
+                    <span> | </span>
+                    {user ?
+                        <>
+                            <span> | </span>
+                            <Link to={'/logout'}>Logout</Link>
+                        </>
+                        : <Link to={'/login'}>Login</Link>
+                    }
+                    {user && <span> | Hello {user.first_name}</span>}
+                </nav>
             </div>
         </div>
     )
