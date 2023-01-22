a82602c942b287e68001122909ca97e9b6abfe89
Aleksandar <aca.peric93@gmail.com>
2023-01-23 00:44:53 +0100

    login fix

--- frontend/pages/LoginPage.jsx
+++ frontend/pages/LoginPage.jsx
@@ -1,28 +1,30 @@
 import React, { useContext } from "react"
-import { useNavigate } from "react-router-dom"
+import { Navigate, redirect } from "react-router-dom"
 import AuthContext from "../context/AuthContext";
 
 const LoginPage = () => {
 
-    const { loginUser } = useContext(AuthContext)
-    const navigate = useNavigate()
+    const { user, loginUser } = useContext(AuthContext)
 
     const handleSubmit = (e) => {
         e.preventDefault();
         loginUser(e.target.username.value, e.target.password.value);
-        navigate('/')
+        return redirect('/')
     }
 
     return (
 
         <div id='container'>
-            <div id="login">
-                <form onSubmit={handleSubmit}>
-                    <input type="text" name="username" placeholder="Enter username..." />
-                    <input type="password" name="password" placeholder="Enter password..." />
-                    <input type="submit" />
-                </form>
-            </div>
+            {user ? <Navigate to='/' /> :
+                (<div id="login">
+                    {/* <h1>Please login first...</h1> */}
+                    <form onSubmit={handleSubmit}>
+                        <input type="text" name="username" placeholder="Enter username..." />
+                        <input type="password" name="password" placeholder="Enter password..." />
+                        <input type="submit" />
+                    </form>
+                </div>)
+            }
         </div>
     )
 }
