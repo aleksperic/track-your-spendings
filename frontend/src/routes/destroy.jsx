995b4c910e0b50c63d30ef8b2026c25c9204c734
Aleksandar <aca.peric93@gmail.com>
2023-01-24 23:31:26 +0100

    destroy route

---
+++ frontend/src/routes/destroy.jsx
@@ -0,0 +1,18 @@
+import { redirect } from "react-router-dom";
+
+const authTokens = JSON.parse(localStorage.getItem('authTokens'))
+
+export async function action({ params }) {
+    const receiptId = params.receiptId
+
+    const response = await fetch(`http://localhost:8000/api/receipt/${receiptId}/delete/`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + authTokens.access
+        },
+    });
+    const data = await response.json()
+    console.log(data);
+    return redirect('/')
+}
