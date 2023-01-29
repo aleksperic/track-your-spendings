01848beacab15c33c7cd1fc0da042abaa4aa2ddf
Aleksandar <aca.peric93@gmail.com>
2023-01-29 23:49:19 +0100

    home page

--- frontend/pages/HomePage.jsx
+++ frontend/pages/HomePage.jsx
@@ -3,14 +3,16 @@ import { Link } from "react-router-dom"
 
 const HomePage = () => {
     return (
-        <>
-            <div>
+        <div id="welcome-page">
+            <h2>
                 Dobrodošli na Skeniraj račun
-            </div>
+            </h2>
             <Link to={'/login'}>
                 <button>Prijavi se</button>
             </Link>
-        </>
+            <h3>Nemate nalog? <Link to={'/register'}>Registruj se</Link>.</h3>
+        </div>
+
     )
 }
 
