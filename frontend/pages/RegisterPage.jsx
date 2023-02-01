816dd6b5fd7ec27e9be5889e953daff382a60e48
Aleksandar <aca.peric93@gmail.com>
2023-02-01 23:51:50 +0100

    register page

--- frontend/pages/RegisterPage.jsx
+++ frontend/pages/RegisterPage.jsx
@@ -3,10 +3,33 @@ import { redirect } from "react-router-dom"
 
 const RegisterPage = () => {
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        return redirect('/')
-    }
+        const data = {
+            first_name: e.target.fname.value,
+            last_name: e.target.lname.value,
+            email: e.target.email.value,
+            username: e.target.username.value,
+            password: e.target.password.value,
+            password2: e.target.password2.value,
+        };
+
+        const response = await fetch("http://localhost:8000/api/users/register/", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        });
+
+        if (response.status === 201) {
+            redirect('/login');
+        } else {
+            console.log(response.statusText);
+            console.error("Failed to register user");
+        }
+    };
+
 
     return (
 
@@ -14,11 +37,11 @@ const RegisterPage = () => {
             <div id="register">
                 <form onSubmit={handleSubmit}>
                     <input type="text" name="fname" placeholder="First name" />
-                    <input type="text" name="flname" placeholder="Last name" />
+                    <input type="text" name="lname" placeholder="Last name" />
                     <input type="email" name="email" placeholder="Email" />
                     <input type="text" name="username" placeholder="Username" />
                     <input type="password" name="password" placeholder="Password" />
-                    <input type="password" name="conf-password" placeholder="Confirm Password" />
+                    <input type="password" name="password2" placeholder="Confirm Password" />
                     <input type="submit" value="Registruj se" />
                 </form>
             </div>
