9a3cf9076fc9ac603365a7e3c8b00a8f3d0f8351
Aleksandar <aca.peric93@gmail.com>
2023-02-07 20:03:03 +0100

    app urls update

--- spending_tracker/urls.py
+++ spending_tracker/urls.py
@@ -16,16 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from users.views import MyTokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/receipt/', include('receipt.urls')),
-    # path('api/auth/', include('users.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/users/', include('users.urls')),
+    path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
