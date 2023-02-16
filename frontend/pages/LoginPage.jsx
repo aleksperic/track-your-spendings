4a546af6164f52626e83de0cd8d5c2866b86cc57
Aleksandar <aca.peric93@gmail.com>
2023-02-16 23:48:52 +0100

    login pages bug fix

--- frontend/pages/LoginPage.jsx
+++ frontend/pages/LoginPage.jsx
@@ -1,16 +1,33 @@
-import React, { useContext } from "react"
+import React, { useContext, useEffect, useState } from "react"
 import { Link, Navigate, redirect } from "react-router-dom"
 import AuthContext from "../context/AuthContext";
 
 const LoginPage = () => {
 
     const { user, loginUser } = useContext(AuthContext)
+    const [msg, setMsg] = useState(null)
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        loginUser(e.target.username.value, e.target.password.value);
-        return redirect('/')
+        const username = e.target.username.value
+        const password = e.target.password.value
+        if (!username || !password) {
+            setMsg('Unesite korisničko ime i lozinku!')
+        } else {
+            loginUser(username, password);
+            return redirect('/')
+        }
     }
+
+    useEffect(() => {
+        let interval = setInterval(() => {
+            if (msg) {
+                setMsg(null)
+            }
+        }, 2000)
+        return () => clearInterval(interval)
+
+    }, [msg])
 
     return (
 
@@ -18,8 +35,9 @@ const LoginPage = () => {
             {user ? <Navigate to='/' /> :
                 (<div id="login">
                     <form onSubmit={handleSubmit}>
-                        <input type="text" name="username" placeholder="Enter username..." />
-                        <input type="password" name="password" placeholder="Enter password..." />
+                        <input type="text" name="username" placeholder="Korisničko ime..." />
+                        <input type="password" name="password" placeholder="Lozinka..." />
+                        {msg && <p style={{ color: 'red', textAlign: 'center' }}>{msg}</p>}
                         <input type="submit" value="Prijavi se" />
                     </form>
                     <h3>Nemate nalog? <Link to={'/register'}>Registruj se</Link>.</h3>
