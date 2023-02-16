5751e32da323851d5da30a12b2c67cbcf361f291
Aleksandar <aca.peric93@gmail.com>
2023-02-16 23:51:14 +0100

    root route refactored

--- frontend/src/routes/root.jsx
+++ frontend/src/routes/root.jsx
@@ -1,7 +1,8 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Outlet, useLoaderData, NavLink, useNavigation } from "react-router-dom";
 import Header from "../../components/Header";
 import AuthContext from "../../context/AuthContext";
+import HomePage from "../../pages/HomePage";
 
 export async function loader() {
 
@@ -30,48 +31,52 @@ export default function Root() {
 
   return (
     <div id='container'>
-      <Header />
-      <div id='content'>
-        <div id="sidebar">
-          <h1>Skeniraj račun</h1>
-          <nav>
-            {user && receipts?.length ? (
-              <ul>
-                <h3 style={{borderBottom: 'solid black'}}>Vaši računi</h3>
-                {receipts.map((receipt) => (
-                  <li key={receipt.id}>
-                    <NavLink to={`receipts/${receipt.id}`}
-                      className={({ isActive, isPending }) =>
-                        isActive
-                          ? "active"
-                          : isPending
-                            ? "pending"
-                            : ""
-                      }>
-                      {receipt.store ? (
-                        <>
-                          {receipt.store.slice(0, 18)}... {receipt.purchase_date}
-                        </>
-                      ) : (
-                        <i>No Name</i>
-                      )}{" "}
-                    </NavLink>
-                  </li>
-                ))}
-              </ul>
-            ) : (
-              <p>
-                <i>Nemate skeniranih računa</i>
-              </p>
-            )}
-          </nav>
-        </div>
-        <div id="detail"
-          className={navigation.state === "loading" ? "loading" : ""}>
-          <h1>{user ? ('Zdravo ' + user.first_name) : ''}</h1>
-          <Outlet />
-        </div>
-      </div>
+      {user ?
+        <>
+          <Header />
+          <div id='content'>
+            <div id="sidebar">
+              <h1>Skeniraj račun</h1>
+              <nav>
+                {user && receipts?.length ? (
+                  <ul>
+                    <h3 style={{ borderBottom: 'solid black' }}>Vaši računi</h3>
+                    {receipts.map((receipt) => (
+                      <li key={receipt.id}>
+                        <NavLink to={`receipts/${receipt.id}`}
+                          className={({ isActive, isPending }) =>
+                            isActive
+                              ? "active"
+                              : isPending
+                                ? "pending"
+                                : ""
+                          }>
+                          {receipt.store ? (
+                            <>
+                              {receipt.store}... {receipt.purchase_date}
+                            </>
+                          ) : (
+                            <i>No Name</i>
+                          )}{" "}
+                        </NavLink>
+                      </li>
+                    ))}
+                  </ul>
+                ) : (
+                  <p>
+                    <i>Nemate skeniranih računa</i>
+                  </p>
+                )}
+              </nav>
+            </div>
+            <div id="detail"
+              className={navigation.state === "loading" ? "loading" : ""}>
+              <h1>{user ? ('Zdravo ' + user.first_name) : ''}</h1>
+              <Outlet />
+            </div>
+          </div>
+        </> : <HomePage />
+        }
     </div>
   );
 }
